6c5700724df47f4a4ff4fdafe5ec76ed97ac3f04
dylpckl <dylanjbsmith@gmail.com>
2022-06-02 14:32:33 -0400

    google analytics

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+
+                </Head>
+                <Script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <Script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                    }}
+                />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
